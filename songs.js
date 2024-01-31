bcfe6e871617c8ac729f9012edd5b678484e2842
Marcelo Santos <107442410+Mpsags@users.noreply.github.com>
2024-01-31 16:44:06 -0400

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,40 @@
+export default [
+
+{
+src:"./src/A Rosa e o Espinho   Rosa de Saron In Concer.mp3",
+name: " Rosa de Saron",
+},
+
+{
+    src:"./src/GABRIELA ROCHA   VIDA AOS SEPULCROS (AO VIVO.mp3" ,
+    name: "Gabriela Rocha - Vida aos Sepulcros",
+    },
+
+          {
+            src: "./src/Livres Para Adorar   Nunca Me Deixou    DVD Mais Um Di.mp3",
+            name: " Livres para Adorar- Nunca Me Deixou",
+            },
+            {
+              src: "./src/De Todas as Tribos   Adoração IBMorumbi (Guilherme Kerr.mp3",
+              name: " Adoração IBM- De Todas as Tribos",
+              },
+                {
+                src: "./src/eRebanhão   PALÁCIOS (Ao Vivo.mp3 ",
+                name: " Rebanhão - PALÁCIOS",
+                },
+                {
+
+                  src:"./src/ESPELHOS MÁGICOS  NAVES IMPERIAIS   MORADA (CLIPE OFICIAL.mp3",
+                  name: " Rebanhão- Espelhos Mágicos",
+                },
+
+{
+src:"./src/Kleber Lucas   Aos Pés Da Cruz (Clipe Oficial MK Music.mp3",
+name: " Kleber Lucas- Aos Pés da Cruz",
+},
+  {
+    src:"./src/kPRIMEIRO AMOR   MORADA (AO VIVO.mp3",
+    name:"MORADA - Primeiro Amor",
+  },
+
+];
